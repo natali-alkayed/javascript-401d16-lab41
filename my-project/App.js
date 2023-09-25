138a6caf2b5d19eae847ead8ec0941b4c277aa1a
natali-alkayed <nataly2000alkayed@gmail.com>
2023-09-25 20:51:48 +0000

    lab 42

--- my-project/App.js
+++ my-project/App.js
@@ -1,106 +1,51 @@
 import React, { useState } from 'react';
-import { StatusBar, StyleSheet, Text, View, TouchableOpacity, ScrollView, TextInput, Button, Vibration } from 'react-native';
+import { StatusBar, StyleSheet, ScrollView, Switch, View, Text } from 'react-native';
+
+import VotingScreen from './components/VotingScreen';
+import ResultsScreen from './components/ResultsScreen';
 
 export default function App() {
   const [options, setOptions] = useState([]);
   const [showResults, setShowResults] = useState(false);
-  const [newOptionText, setNewOptionText] = useState('');
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const handleVote = (index) => {
-    const updatedOptions = [...options];
-    updatedOptions[index].votes += 1;
-    setOptions(updatedOptions);
-
-    Vibration.vibrate(500);
-  };
-
-  const handleDeleteOption = (index) => {
-    const updatedOptions = [...options];
-    updatedOptions.splice(index, 1);
-    setOptions(updatedOptions);
-  };
-
-  const calculatePercentage = (votes) => {
-    const totalVotes = options.reduce((sum, option) => sum + option.votes, 0);
-    return totalVotes === 0 ? 0 : ((votes / totalVotes) * 100).toFixed(2);
-  };
-
-  const renderOptions = () => {
-    return options.map((option, index) => (
-      <View key={index} style={styles.optionContainer}>
-        <TouchableOpacity
-          style={styles.option}
-          onPress={() => handleVote(index)}>
-          <Text>{option.text}</Text>
-          <Text>{option.votes} votes</Text>
-        </TouchableOpacity>
-        <Button
-          title="Delete"
-          onPress={() => handleDeleteOption(index)}
-          color="red"
-        />
-      </View>
-    ));
-  };
-
-  const renderResults = () => {
-    return options.map((option, index) => (
-      <View key={index} style={styles.result}>
-        <Text>{option.text}:</Text>
-        <Text>{option.votes} votes</Text>
-        <Text>{calculatePercentage(option.votes)}%</Text>
-      </View>
-    ));
-  };
-
-  const addOption = () => {
-    if (newOptionText.trim() === '') {
-      return;
-    }
-    const newOption = { text: newOptionText, votes: 0 };
-    setOptions([...options, newOption]);
-    setNewOptionText('');
-  };
-
-  const resetVotes = () => {
-    const resetOptions = options.map((option) => ({
-      ...option,
-      votes: 0,
-    }));
-    setOptions(resetOptions);
-    setShowResults(false);
+  // Toggle dark mode function
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
   };
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text>Vote for your favorite option:</Text>
-      {options.length === 0 && (
-        <Text>No options available. Add some options below:</Text>
-      )}
+    <ScrollView
+      contentContainerStyle={[
+        styles.container,
+        isDarkMode ? styles.darkContainer : styles.lightContainer,
+      ]}
+    >
+      <View style={styles.header}>
+        <Text style={[styles.headerText, isDarkMode ? styles.darkHeaderText : {}]}>Voting App</Text>
+        <Switch
+          value={isDarkMode}
+          onValueChange={toggleDarkMode}
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={isDarkMode ? "#f5dd4b" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+        />
+      </View>
+
       {!showResults ? (
-        <View>
-          {renderOptions()}
-          <TextInput
-            placeholder="Enter a new option"
-            value={newOptionText}
-            onChangeText={(text) => setNewOptionText(text)}
-            style={styles.input}
-          />
-          <View style={styles.buttonContainer}>
-            <Button title="Add Option" onPress={addOption} />
-            <Button title="View Results" onPress={() => setShowResults(true)} />
-          </View>
-          <Button title="Reset Votes" onPress={resetVotes} />
-        </View>
+        <VotingScreen
+          options={options}
+          setOptions={setOptions}
+          isDarkMode={isDarkMode}
+          styles={isDarkMode ? styles.darkText : styles.text} // Apply text style
+        />
       ) : (
-        <View style={styles.resultsContainer}>
-          <Text style={styles.resultsTitle}>Results:</Text>
-          {renderResults()}
-          <Button
-            title="Back to Voting"
-            onPress={() => setShowResults(false)}
-          />
-        </View>
+        <ResultsScreen
+          options={options}
+          setShowResults={setShowResults}
+          isDarkMode={isDarkMode}
+          styles={isDarkMode ? styles.darkText : styles.text} // Apply text style
+        />
       )}
       <StatusBar style="auto" />
     </ScrollView>
@@ -110,49 +55,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingVertical: 20,
+    alignItems: 'center',
+  },
+  lightContainer: {
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  optionContainer: {
+  darkContainer: {
+    backgroundColor: '#000',
+  },
+  text: {
+    color: 'black', // Change text color for light mode
+  },
+  darkText: {
+    color: 'white', // Change text color for dark mode
+  },
+  header: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginVertical: 5,
+    paddingHorizontal: 20,
   },
-  option: {
-    flex: 1,
-    backgroundColor: '#e0e0e0',
-    padding: 10,
-    marginEnd: 5,
-    borderRadius: 5,
-  },
-  result: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 10,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 10,
-  },
-  resultsContainer: {
-    backgroundColor: '#f0f0f0',
-    padding: 20,
-    borderRadius: 10,
-    marginTop: 20,
-  },
-  resultsTitle: {
-    fontSize: 18,
+  headerText: {
+    fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 10,
   },
-  buttonContainer: {
-    flexDirection: 'column',
-    alignItems: 'center',
-    marginVertical: 10,
+  darkHeaderText: {
+    color: 'white', // Change text color for dark mode
   },
 });
---
+++ my-project/READMElab42.md
@@ -0,0 +1,22 @@
+# Voting App
+
+The Voting App is a simple mobile application that allows users to create and participate in polls or voting sessions. It offers a user-friendly interface for creating and managing voting options and displays results in real-time.
+
+## Features
+
+### 1. Create and Manage Voting Options
+- Users can easily create new voting options by entering text in the provided input field.
+- Options can be added to the list, allowing users to customize the voting choices.
+- Options are displayed with a vote count, and users can vote for their favorite choice.
+
+### 2. Real-time Voting Results
+- As users cast their votes, the app keeps track of the total votes for each option and updates the results in real-time.
+- The results are presented as a percentage of total votes for each option.
+
+### 3. Dark Mode Support
+- The app features a dark mode that enhances visibility and reduces eye strain in low-light environments.
+- Users can switch between light and dark modes using the toggle switch provided in the header.
+
+### Photos : 
+![Alt text](lab42.jpg)
+![Alt text](lab422.jpg)
